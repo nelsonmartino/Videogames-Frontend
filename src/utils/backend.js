121167780031a716bc5ab9b8a487fb4b805c7ed4
nelsonmartino <nelsonmartino@gmail.com>
2023-11-17 00:07:57 -0300

    deploy mods

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -1,1 +1,3 @@
-export const URL_BACKEND = "https://videogames-backend-fet8.onrender.com";
+// export const URL_BACKEND = "https://videogames-backend-fet8.onrender.com";
+export const URL_BACKEND =
+  'http://videogames-backend-production-d8b0.up.railway.app';
